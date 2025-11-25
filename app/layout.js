7542136692e07223256a88403b0272a1150c13e2
Raviteja7606 <ravitejatattikolla@gmail.com>
2025-11-25 19:23:40 +0530

    Refactor layout to include ToastProvider and GTM

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,13 @@
-import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
-import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
+import ScrollToTop from "./components/helper/scroll-to-top";
+import ToastProvider from "./components/ToastProvider";
+import GTM from "./components/GTM";
+
 import "./css/card.scss";
 import "./css/globals.scss";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -19,15 +20,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
+        <ToastProvider />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
           <ScrollToTop />
         </main>
         <Footer />
+
+        {/* GTM client component */}
+        <GTM />
       </body>
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }
